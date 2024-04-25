c485e7afac563731b4af0f5aa6163b83acd3397d
Munta101 <ngandamutwalibu@proton.me>
2024-04-25 17:11:40 +0300

    added code that connects to wifi network

--- esp32_code_space.py
+++ esp32_code_space.py
@@ -16,3 +16,34 @@ def scan_wifi_networks():
 
 # Call the function to scan for Wi-Fi networks
 scan_wifi_networks()
+
+# Wokwi SSID
+# Wokwi-GUEST
+
+# CODE TO CONNECT TO WIFI NETWORK
+import network
+import time
+
+# Connect to Wi-Fi
+def connect_to_wifi(ssid, password):
+    station = network.WLAN(network.STA_IF)
+    if station.isconnected():
+        print("Already connected to Wi-Fi:", station.ifconfig())
+        return
+
+    station.active(True)
+    station.connect(ssid, password)
+
+    print("Connecting to Wi-Fi...")
+    while not station.isconnected():
+        pass
+
+    print("Connected to Wi-Fi:", station.ifconfig())
+
+
+# Replace with your Wi-Fi network credentials
+wifi_ssid = "Wokwi-GUEST"
+wifi_password = ""
+
+# Connect to Wi-Fi
+connect_to_wifi(wifi_ssid, wifi_password)
